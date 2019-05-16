d323bb1f5d907a27bc06d5e50a1825d07f7afd79
christophetrannoy <christophe.trannoy@gmail.com>
2019-05-16 09:40:00 +0200

    Event

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :events
   get 'welcome/index'
   resources :articles do
     resources :comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_185021) do
+ActiveRecord::Schema.define(version: 2019_05_16_071027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2019_05_14_185021) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "startDate"
+    t.date "endDate"
+    t.text "eventPlace"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
